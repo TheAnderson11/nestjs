a8f5f7bdd30d9fe357c1fd6ec855228992f14514
TheAnderson11 <andersonshtf3311@gmail.com>
2025-04-17 23:33:43 +0300

    initial repository

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { SequelizeModule } from '@nestjs/sequelize'
+import configurations from 'src/configurations'
+import { UserModule } from '../user/user.module'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+
+@Module({
+  imports: [
+    UserModule,
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get('db_host'),
+        port: configService.get('db_port'),
+        username: configService.get('db_user'),
+        password: configService.get('db_password'),
+        database: configService.get('db_name'),
+        synchronize: true,
+        autoLoadModels: true,
+        models: [],
+      }),
+    }),
+    ConfigModule.forRoot({ isGlobal: true, load: [configurations] }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
